b6beb176702771767793908837e45d12945e4f58
CoderzHub <lakshyaai@outlook.com>
2024-08-28 20:18:47 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("This is the Tic Tac Toe Unbeatable machine")
+boardArr = [1,2,3,4,5,6,7,8,9]
+board= f"{boardArr[1-1]} | {boardArr[2-1]} | {boardArr[3-1]} \n ----------- \n {boardArr[4-1]} | {boardArr[5-1]} | {boardArr[6-1]} \n ----------- \n {boardArr[7-1]} | {boardArr[8-1]} | {boardArr[9-1]} "
+print(f"The starting board:\n {board}\n\n Let's Start:")
